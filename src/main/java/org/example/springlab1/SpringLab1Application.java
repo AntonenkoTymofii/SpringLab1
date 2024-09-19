427690d658ed4ba6f1ad2116e7ed354d14bda900
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-09-19 18:20:48 +0300

    finish first task

--- src/main/java/org/example/springlab1/SpringLab1Application.java
+++ src/main/java/org/example/springlab1/SpringLab1Application.java
@@ -1,13 +1,20 @@
 package org.example.springlab1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringLab1Application {
+public class SpringLab1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		System.out.println("Start Spring Lab 1");
 		SpringApplication.run(SpringLab1Application.class, args);
+		System.out.println("End Spring Lab 2");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World from Spring Lab");
+	}
 }
